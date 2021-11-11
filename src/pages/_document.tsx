d972f635b35fff0d3e77ae3e8b31dd9bc0ff42f4
HigorDenomar <higordenomar@hotmail.com>
2021-11-11 13:00:55 -0300

    document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,18 @@
-import Document from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
